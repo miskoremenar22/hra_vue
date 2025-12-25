9f83a5ae5bc5fcc8dfe1f915c562920ca64918d4
Michal <miskoremenar@gmail.com>
2025-12-25 18:49:42 +0100

    PWA hotova

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,48 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // TOTO ZABEZPEČÍ, ŽE PWA FUNGUJE AJ NA LOCALHOSTE (uvidíš tú šípku)
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'Master Chef Game',
+        short_name: 'MasterChef',
+        description: 'Zábavná kuchárska hra vo Vue.js',
+        theme_color: '#ff9800',
+        background_color: '#fdf5e6',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
